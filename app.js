9dbc6fbdad0dc8815ae78414936970d9a804d55c
Dolly <dollyshrivas2005@gmail.com>
2026-09-24 20:30:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ PORT = 3000;
 //cors
 app.use(
   cors({
-    origin:"https://shrijicarwash.vercel.app/",
+    origin:"https://shrijicarwash.vercel.app",
     credentials:true,
     methods:["GET","POST","PUT","DELETE"],
   })
