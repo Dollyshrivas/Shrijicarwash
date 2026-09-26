50ad9d232a785fc72929861572b22eb4f2248ce3
Dolly <dollyshrivas2005@gmail.com>
2026-09-26 16:45:47 +0530

    Update Profile.jsx

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { apiRequest } from "../lib/api";
+import { apiRequest } from "../api";
 
 function Profile() {
   const [user, setUser] = useState(() =>
